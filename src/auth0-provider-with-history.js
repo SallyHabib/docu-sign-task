160cf44b6371ae50255137016788a9f6a2047a62
SallyHabib <sallyhabib52@gmail.com>
2020-09-19 23:20:30 +0200

    Update auth0-provider-with-history.js

--- src/auth0-provider-with-history.js
+++ src/auth0-provider-with-history.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const Auth0ProviderWithHistory = ({ children }) => {
+  //the domain and the clientId should be added in env files but I have added them in the files directly for testing purposes
   const domain = "dev-635toj-8.us.auth0.com";
   const clientId ="yO9c56xPIVL8Bd8m2jRjBYuNWkejc7sl";
 
